7c68711c49b116b3615ad73098b11467ff993030
Mikhail Kurbanov <slowwwdowns@gmail.com>
2023-03-23 16:59:39 +0300

    Refactoring№2

--- src/test/java/FirstTestGuru.java
+++ src/test/java/FirstTestGuru.java
@@ -1,16 +1,16 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.Test;
 
 public class FirstTestGuru {
 
     @Test
     void firstTest() {
-        Assertions.assertFalse(3 < 2);
+        Assertions.assertTrue(3 > 2);
     }
 
 
     @Test
     void secondTest() {
-        Assertions.assertFalse(3 < 1);
+        Assertions.assertTrue(3 > 1);
     }
 }
